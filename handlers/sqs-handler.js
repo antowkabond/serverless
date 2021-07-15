fdf939635e145d070461c6125b5378e1275178a1
antowkabond <antowkabond1@gmail.com>
2021-07-15 08:26:11 +0300

    fixes

--- handlers/sqs-handler.js
+++ handlers/sqs-handler.js
@@ -1,7 +1,6 @@
 'use strict';
 
 exports.consume = async (event) => {
-    console.log(event.Records);
     const [record] = event.Records;
 
     const message = `Consume record ${JSON.stringify(record[0])}`;
